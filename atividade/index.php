f7a856d599cdd4bda46c365721c4e15bce753305
IsabellaCarolinesc <92828539+IsabellaCarolinesc@users.noreply.github.com>
2022-03-17 14:12:51 -0300

    Add files via upload

---
+++ atividade/index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Atividade 2022</title>
+		
+		
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      
+      <a class="navbar-brand" href="#">Sobre</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Dados Pessoais</a></li>
+        <li><a href="#">Objetivos</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div style="height: 6px; background: #FF8C00;"></div>
+
+<div class="jumbotron text-center">
+<div class="container-fluid bg-2 text-center">
+
+<img src="img/eu.PNG" height="500px">
+  <h2 class="margin">Isabella Caroline Da Silva Carvalho</h2>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script> 
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
+});
+</script>
+
+<style> 
+#panel, #flip {
+  padding: 5px;
+  text-align: center;
+  background-color: #F8F8F8;
+  border: solid 1px #F8F8F8;
+}
+
+#panel {
+  padding: 50px;
+  display: none;
+}
+</style>
+
+<div id="flip">  <button type="button" class="btn btn-warning">
+Continuar</button></div>
+
+
+<div id="panel">
+<div id="area-principal">
+<div id="area-postagem">
+<div class="postagem">
+  
+  
+<div class="container">
+<div class="row">
+
+<div class="row">
+      
+     <p>Sobre mim:</p>
+  
+ <b>Olá, me chamo Isabella, tenho 18 anos e estou cursando o útimo ano do Ensino Médio, junto ao Curso Técnico de Informática para Internet, no Instituto Federal de Guarulhos.
+ Durante minha trajetória acadêmica tenho exercido várias atividade extracurriculares. Como na participação de projetos para cursos de desenvolvimento web para adolescentes, e desenvolvimento de chatbot's,
+  que teve grande destaque em algumas feiras culturais na cidade de Guarulhos, no ano de 2020 e 2021.</b>
+
+ </div></div>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head></br>
+
+<center>
+<div class="row">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").toggle();
+  });
+});
+</script>
+
+ <button type="button" class="btn btn-warning">
+Clique para saber mais:</button></br>
+                                   <br><br>
+                        
+                        <div style="height: 2px; background: #FF8C00;"></div>
+                        
+                        <br><p>DADOS PESSOAIS:</p>
+                        
+                        <b>Nome:</b> Isabella Caroline da Silva Carvalho.
+
+                        <br><b>E-mail:</b> Isabellacarvalho865@gmail.com.
+
+                        <br><b>Data de nascimento:</b> 03/07/2003.
+
+                        <br><b>Cidade:</b> Guarulhos.
+
+                        <br><b>Telefone:</b> (11) 97324-4958.
+                        <br><br>
+
+                        <p>&nbsp;
+
+                        <br>
+                        <div style="height: 2px; background: #FF8C00;"></div><br>
+                        <p>DADOS ACADÊMICOS:</p>
+                        <b>Tipo de formação:</b> FUNDAMENTAL.
+                        <br><b>Curso:</b> Ensino Fundamental.
+                        <br><b>Instituição:</b> Colégio Serrano Guardia. 
+                        <br><b>Data de Conclusão:</b> 12/2018.
+                        <br><br>
+                        
+                        <br><b>Tipo de formação:</b> ESPANHOL.
+                        <br><b>Curso:</b> Lingua Espanhola.
+                        <br><b>Instituição:</b> Colégio Serrano Guardia. 
+                        <br><b>Data de Conclusão:</b> 12/2018.
+                        <br><br>
+                        
+                        <br><b>Tipo de formação:</b> INFORMÁTICA.
+                        <br><b>Curso:</b> Introdução a informática e suas tecnologias.
+                        <br><b>Instituição:</b> Colégio Serrano Guardia. 
+                        <br><b>Data de Conclusão:</b> 12/2018.
+                        <br><br>
+                        
+                        <br><b>Tipo de formação:</b> HTML 5.
+                        <br><b>Curso:</b> Construindo a Internet.
+                        <br><b>Instituição:</b> Colégio Serrano Guardia. 
+                        <br><b>Data de Conclusão:</b> 12/2018.
+                        <br><br>
+
+                        <br><b>Tipo de formação:</b> HTML 5 CSS 3.
+                        <br><b>Curso:</b> Desenvolvimento web.
+                        <br><b>Instituição:</b> Instituto Federal De Educação, Ciência e Tecnologia Campus Guarulhos. 
+                        <br><b>Data de Conclusão:</b> 12/2019.
+                        <br><br>
+                       
+                        <br><b>Tipo de formação:</b> ENSINO MÉDIO.
+                        <br><b>Curso:</b> Tecnico em Informática para Internet Integrado ao Ensino Médio.
+                        <br><b>Instituição:</b> Instituto Federal De Educação, Ciência e Tecnologia Campus Guarulhos. 
+                        <br><b>Data de Conclusão:</b> 12/2022 - Cursando.
+                        <br><br>
+                        
+                        <p>&nbsp;
+                        
+                         <br>
+                        <div style="height: 2px; background: #FF8C00;"></div><br>
+                        <p>Experiências:</p>
+                        <b>Tipo de experiência:</b> Estágio de Desenvolvimento Web.
+                        <br><b>Período de atuação:</b> Setembro de 2020 à 2021. <br>
+                        <br><b>Instituição:</b> Bolsista De extensão X Instituto Federal de Educação Ciência e Tecnologia Campus Guarulhos.<br>
+                        <br><b>Trabalhos realizados:</b> Acompanhei o desenvolvimento de projetos voltados para a inclusão feminina no ramo tecnologico, através da elaboração de conteúdos via canais digitais como o Instagram. 
+Colaborei como ministrante de oficinas em feiras culturais, onde foram elaborados projetos de desenvolvimento web e chat bot. 
+Ministrei aulas de desenvolvimento web e informática básica para idosos.                        <br><br>
+
+                        
+
+
+</div>
+</center>
+</div></div></div>
+
+
+</div>
+
+<br>
+<div style="height: 6px; widht:100px; background: #FF8C00;"></div>
+
+
+<style type="text/css">
+		
+#area-principal{
+  width: 860px;
+  margin: 0px auto;
+  border: 2px solid black;
+  padding: 10px;
+  border-radius: 25px;
+}
+}
+
+#area-postagem{
+  background:#DCDCDC;
+}
+
+.postagem{
+  padding: 20px;
+  margin-bottom: 20px;
+  background: #DCDCDC; 
+  border-radius: 25px;
+}
+
+img {
+  float: center;
+  width:100px;
+  height:100px;
+}
+
+h2{
+padding:15px;
+color:	#A9A9A9;
+font-family:Helvetica Neue, Helvetica, Arial; }
+
+h4{
+text-aling:right;}
+}
+
+
+	
+</div>
+</body>
+</html>
